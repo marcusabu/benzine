121f559d9cd8073af607d6661ea80b5f6ec80d0f
Marcus Abukari <marcusabu@gmail.com>
2024-10-01 11:56:19 +0200

    Move analytics to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Analytics />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
